29b45a35f955da27d5d31d5ab8131886a77bada1
Lam Nguyen <lam.sustech@gmail.com>
2024-05-20 17:43:07 +0800

    first commit

---
+++ optimizer.py
@@ -0,0 +1,102 @@
+from typing import Callable, Iterable, Tuple
+import math
+
+import torch
+from torch.optim import Optimizer
+
+
+class AdamW(Optimizer):
+    def __init__(
+            self,
+            params: Iterable[torch.nn.parameter.Parameter],
+            lr: float = 1e-3,
+            betas: Tuple[float, float] = (0.9, 0.999),
+            eps: float = 1e-6,
+            weight_decay: float = 0.0,
+            correct_bias: bool = True,
+    ):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[1]))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
+        super().__init__(params, defaults)
+
+    def step(self, closure: Callable = None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+
+                # State should be stored in this dictionary
+                state = self.state[p]
+
+                # Access hyperparameters from the `group` dictionary
+                alpha = group["lr"]
+
+                # Complete the implementation of AdamW here, reading and saving
+                # your state in the `state` dictionary above.
+                # The hyperparameters can be read from the `group` dictionary
+                # (they are lr, betas, eps, weight_decay, as saved in the constructor).
+                #
+                # 1- Update first and second moments of the gradients
+                # 2- Apply bias correction
+                #    (using the "efficient version" given in https://arxiv.org/abs/1412.6980;
+                #     also given in the pseudo-code in the project description).
+                # 3- Update parameters (p.data).
+                # 4- After that main gradient-based update, update again using weight decay
+                #    (incorporating the learning rate again).
+
+                # State initialization
+                if len(state) == 0:
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(p.data)
+
+                exp_avg = state['exp_avg']
+                exp_avg_sq = state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+                eps = group["eps"]
+                weight_decay = group["weight_decay"]
+                correct_bias = group["correct_bias"]
+
+                state['step'] += 1
+                step = state['step']
+
+                # Update first and second moment running average
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                # Bias correction
+                if correct_bias:
+                    bias_correction1 = 1 - beta1 ** step
+                    bias_correction2 = 1 - beta2 ** step
+                    corrected_exp_avg = exp_avg / bias_correction1
+                    corrected_exp_avg_sq = exp_avg_sq / bias_correction2
+                else:
+                    corrected_exp_avg = exp_avg
+                    corrected_exp_avg_sq = exp_avg_sq
+
+                # Update parameters
+                denom = corrected_exp_avg_sq.sqrt().add_(eps)
+                step_size = alpha
+                p.data.addcdiv_(corrected_exp_avg, denom, value=-step_size)
+
+                # Weight decay
+                if weight_decay != 0:
+                    p.data.add_(p.data, alpha=-weight_decay * step_size)
+
+        return loss
